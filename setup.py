8fbe139d70521b4526ce89e3cde259728437d5c2
Jake <jakefaulkn@gmail.com>
2018-04-11 17:43:41 +1200

    chore: add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='clipsync',
+    version='0.1',
+    py_modules=['clipsync'],
+    install_requires=['Click', 'pygobject'],
+    entry_points='''
+        [console_scripts]
+        clipsync=clipsync.clipsync:cli
+    ''',
+)
